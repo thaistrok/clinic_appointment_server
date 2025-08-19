c53304aea87d1b55fd04e94995e03e3340f52dd6
thaistrok <thaistrok@gmail.com>
2025-08-19 15:07:47 +0300

    don

--- FrontEnd/src/services/appointmentUtils.js
+++
@@ -1,8 +0,0 @@
-export const validateAppointmentId = (id, action) => {
-  if (!id) {
-    alert('Invalid appointment ID');
-    console.error(`Cannot ${action} appointment: ID is missing or invalid`, id);
-    return false;
-  }
-  return true;
-};
